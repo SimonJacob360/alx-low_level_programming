5ad8672a0fbab4b5bd4ef13cae578036293b7172
SimonJacob360 <simon.jaocb360@gmail.com>
2023-02-22 08:13:15 -0800

    0_putchar.c

--- 0x02-functions_nested_loops/0_putchar.c
+++ 0x02-functions_nested_loops/0_putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0
+ * main - prints _putchar and a new line
+ * Return Always 0
  */
-
 int main(void)
 {
-
 	_putchar('_');
-	_putchar('p');
-	_putchar('y');
+	_putchsr('p');
+	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar(\n');
+	-putchar('\n');
+
 	return (0);
-}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la ='a'; la <= 'z'; la++)
+		_putchar(la);
+
+	putchar('\n');
+
