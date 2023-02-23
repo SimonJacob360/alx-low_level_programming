3c0fcc23d9a920d0198d9e2f646b877e0951af14
SimonJacob360 <simon.jaocb360@gmail.com>
2023-02-23 06:44:00 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,9 +8,7 @@
 int main(void)
 {
 	int i;
-
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 
 /*
@@ -20,6 +18,5 @@ int main(void)
  * }
  */
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
