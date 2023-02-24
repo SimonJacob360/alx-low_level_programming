74bbdbb5593db37b886a62eb4d4ec1157f947876
SimonJacob360 <simon.jaocb360@gmail.com>
2023-02-23 20:06:18 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifindef MAIN_H
+#define MAIN_H
+
+int putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbrs(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print line(int n);
+void print_diagonal(int o);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
